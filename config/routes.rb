80d5111ba78856d63ab319a089834e0e1237a4c5
Srija S <srijakumar7@gmail.com>
2019-06-03 01:23:58 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   end
 
   resources :posts, only: %i[index show new create edit update]
-
-  get '/admin/stats', to: 'stats#index'
-
+  scope '/admin' do
+    resources :stats, only: [:index]
+  end
   root 'posts#index'
 end
